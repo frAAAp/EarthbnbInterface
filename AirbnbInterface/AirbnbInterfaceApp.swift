bacfd1f3521ffdee3b46c00b4792a35f0aabbc2e
Francesca Pia Gargiulo <francescapiagargiulo19@gmail.com>
2023-11-14 16:07:42 +0100

    Initial Commit

---
+++ AirbnbInterface/AirbnbInterfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbInterfaceApp.swift
+//  AirbnbInterface
+//
+//  Created by Francesca Pia Gargiulo on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbInterfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AirbnbInterface/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AirbnbInterface
+//
+//  Created by Francesca Pia Gargiulo on 14/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
